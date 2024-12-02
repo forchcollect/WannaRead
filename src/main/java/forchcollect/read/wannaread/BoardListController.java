3b0fdfba035b376b328809e5e360752726ada709
forchcollect <forchcollect@naver.com>
2024-12-02 23:01:26 +0900

    간단히 doGet, doPost 이동

--- src/main/java/forchcollect/read/wannaread/BoardListController.java
+++ src/main/java/forchcollect/read/wannaread/BoardListController.java
@@ -1,26 +1,21 @@
 package forchcollect.read.wannaread;
 
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
-@WebServlet(name = "myServlet", urlPatterns = "/my")
-public class MyServlet extends HttpServlet {
+@WebServlet(name = "boardListController", urlPatterns = "/board/list")
+public class BoardListController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        resp.setContentType("text/html");
+        System.out.println("doGet");
 
-        PrintWriter out = resp.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>My Servlet</h1>");
-        out.println("</body></html>");
-
+        req.getRequestDispatcher("/WEB-INF/board/list.jsp")
+                .forward(req, resp);
     }
 }
